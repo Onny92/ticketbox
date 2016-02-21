a0935d072c6ade3190c057af32e8ffa49be66743
Onny <onnytau@gmail.com>
2016-02-22 00:48:01 +0200

    add file:

---
+++ config/session/index.js
@@ -0,0 +1,18 @@
+(function(){
+
+    /**var session = require('express-session');
+    var sessionStore = require('connect-mongo')(session);
+    var db = require(_ + 'config/connection');
+
+    module.exports = session({
+
+              name: 'ticketbox.sid',
+              secret: 'million jedis paying road usage tax',
+              resave: false,
+              // Forces a session that is new but not modified to be saved to the store.
+              saveUninitialized: false,
+              store: new sessionStore({ mongooseConnection: db})
+
+        });**/
+
+})();
